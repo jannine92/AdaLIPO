4121f43600fa5a2bd4c3e76f0ff0d48bb6d3dfe3
Jannine <jannine.schreiber@t-online.de>
2019-04-20 15:07:41 +0200

    Add AdaLIPO files

---
+++ AdaLIPO.py
@@ -0,0 +1,256 @@
+import numpy as np
+import random
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+import timeit
+import time
+import operator
+import decimal
+
+import os
+import pickle
+import shutil
+
+# import gpflow
+# from gpflow import kernels
+
+#import import_ipynb
+#import data_management as data_management
+from sklearn.preprocessing import StandardScaler
+
+import Models.cmaes as cmaes
+#import cmaes as cmaes
+
+
+# Test
+
+
+# images,labels = data_management.get_data(fn = "Input_Dataset/input_data.csv",samples=85) 
+# sc = StandardScaler()
+# images = sc.fit_transform(np.float64(images))
+
+
+def timerfunc(func):
+    """
+    A timer decorator
+    """
+    def function_timer(*args, **kwargs):
+        """
+        A nested function for timing other functions
+        """
+        start = time.time()
+        value = func(*args, **kwargs)
+        end = time.time()
+        runtime = np.float(end - start)
+        msg = "The runtime for {func} took {time} seconds to complete"
+        print(msg.format(func=func.__name__,
+                         time=runtime))
+        
+        return value
+    return function_timer
+
+
+#@timerfunc
+def sum_of_squares_hp():
+    return np.array([[-20,20],[-20,20],[-20,20],[-20,20],[-20,20]])
+
+
+
+def ackley_hp():
+    return np.array([[-32.768,32.768],[-32.768,32.768]])
+    #return [np.arange(-32.768, 32.768), np.arange(-32.768, 32.768)] # evaluation on hypercube
+
+
+
+# def get_kernel(hp):
+#     latents = int(hp[0])
+    
+#     k = [kernels.RBF(input_dim=latents, ARD=True),kernels.Periodic(input_dim=latents),
+#          kernels.Linear(input_dim=latents),kernels.White(input_dim=latents)]
+    
+#     kern = [k[i-1] for i in range(1,4) if hp[i] >= 0.5]
+    
+#     # if all values are < 0.5
+#     if len(kern) == 0:
+#         max_hp = np.argmax(hp[1:4])
+#         kernel = k[max_hp]
+#     else:
+#         kernel = kernels.Sum([kern[i] for i in range(len(kern))])
+    
+#     return kernel
+
+
+# ### Objective Functions
+
+
+#@timerfunc
+def sum_of_squares(hyperparameters): # minimum: 0
+    # 1D
+    #return np.sum(x**2 for x in [hyperparameters])
+    return np.sum([x**2 for x in hyperparameters])
+
+
+
+def ackley(x): # minimum: 0
+        arg1 = -0.2 * np.sqrt(0.5 * (x[0] ** 2 + x[1] ** 2))     
+        arg2 = 0.5 * (np.cos(2. * np.pi * x[0]) + np.cos(2. * np.pi * x[1]))
+        return -20. * np.exp(arg1) - np.exp(arg2) + 20. + np.e
+
+
+# ### Optimizer
+
+
+# Lipschitz function
+def L(xp_t1,fx,X,k):
+    L_xp_t1 = np.amax([fx[index] - k * np.linalg.norm((xp_t1 - X[index]), ord=2) 
+                    for index in range(len(X))])
+    return L_xp_t1
+
+
+
+def meta_L(xp,fx,X,k):
+    return np.array([L(xp_t1,fx,X,k) for xp_t1 in xp])
+
+
+
+@timerfunc
+def adaptive_lipo(n, t_max, hyperparameters, f):
+    """
+    Args:
+        n: number of iterations
+        hyperparameters: possible inputs of f (or bounds for hyperparameters?)
+        f: objective function 
+        p: probability of exploitation vs (1-p) probability of exploration
+    Returns:
+        The estimated value that maximizes f
+    """
+
+    use_cmaes = True
+    
+    # Initialization 
+    min_bounds,max_bounds = np.array(hyperparameters).T
+    x1 = np.array((np.random.uniform(low=min_bounds, high=max_bounds, size=len(hyperparameters)))) 
+    
+    X = [x1]
+    fx = [f(x1)]
+    k = [0]
+    t = 0
+    
+    # Iterations
+    overall_time = []
+    overall_rs_time = []
+    time_k_update = []
+    while t < t_max: 
+        start_time = time.time()
+        
+        r_val = []
+        # sample from L(x)
+        
+        if use_cmaes:
+
+            gen = cmaes.minimize(meta_L, hyperparameters, fx, X, k[t], number_samples=20)
+
+            cmaes_time = []
+            for i in range(n): # TODO: exit criterion 
+                start_time_rs = time.time()
+                params,evaluation = next(gen)
+                #print("Params: ", params, " Evaluation: ", evaluation)
+                #print('len r_val: ', r_val)
+#                 if len(r_val) > 0:
+#                     if np.absolute(r_val[-1][1] - evaluation) < 0.001:
+#                         print('finished after ', i, ' iterations')
+#                         break
+                r_val.append((params,evaluation))
+            
+                end_time_rs = time.time()
+                time_needed_rs = end_time_rs - start_time_rs
+                cmaes_time.append(time_needed_rs)
+                overall_rs_time.append(time_needed_rs)
+                #print('time taken for cmaes loop', i, ' : ', time_needed_cmaes)
+        #         plt.plot(np.arange(0,n),cmaes_time,marker='o', color='deepskyblue')
+        #         plt.title('time for cmaes loop')
+        #         plt.show()
+        
+        else:
+            for a in range(0,n):
+                start_time_rs = time.time()
+
+                xp_t1 = np.array((np.random.uniform(low=min_bounds, high=max_bounds, size=len(hyperparameters)))) 
+
+                # function to minimize
+                L_xp_t1 = np.amax([fx[index] - k[t] * np.linalg.norm((xp_t1 - X[index]), ord=2)
+                                   for index in range(len(X))])
+
+                r_val.append((xp_t1,L_xp_t1))
+                
+                end_time_rs = time.time()
+                time_needed_rs = end_time_rs - start_time_rs
+                overall_rs_time.append(time_needed_rs)
+                
+        # sort after L_xp_t1 and choose the xp_t1 value with the smallest L value
+        best_x = sorted(r_val,key=lambda x: x[1])[0][0]
+        X.append(best_x) 
+        
+        
+        if len(X) >= (t+2):
+            #calc_f_time = time.time()
+            fx.append(f(X[t+1]))
+            #calc_f_time_end = time.time()
+            #print("Time needed to calculate f for timestep", t, " : ", calc_f_time_end - calc_f_time)
+            
+            t += 1
+            
+            start_k_update = time.time()
+            k.append(np.amax([(np.absolute(fx[i]-fx[j])/np.linalg.norm((X[i]-X[j]), ord=1)) #try: ord=2
+                              for i in range(len(X)) for j in range(len(X)) if i != j 
+                              and np.linalg.norm(X[i]-X[j], ord=1)!=0 
+                             ])) # slope of all X's in X
+
+            print("t:{}, k:{} with x:{} and obj:{}".format(t-1,k[t],X[t],fx[t]))
+            end_time = time.time()
+            time_loop = end_time - start_time
+            time_k_update.append(end_time-start_k_update)
+            overall_time.append(time_loop)
+    if use_cmaes:            
+        rs_time_reshaped = np.array(overall_rs_time).reshape((t_max,n))
+        rs_time_sum = []
+        for i in range(0,t_max):
+            rs_time_sum.append(np.sum(rs_time_reshaped[i]))
+    else:
+        rs_time_sum = overall_rs_time
+    
+    min_obj = np.argmin(fx) # model position of minimum object
+    print('Minimum: ', np.amin(fx), ' for X: ', X[min_obj], ' with k: ', k[min_obj])
+    return min_obj, X[min_obj]
+    
+#     models_dir = './Models/gplvm_models/'
+#     models_filename = models_dir + str(min_obj)
+    
+#     fm_dir = './Models/Trained_Models/'
+    
+#     files = os.listdir(fm_dir) 
+#     number_files = len(files)
+#     fm_filename = fm_dir + str(number_files-1) #+1?
+#     while os.path.isfile(fm_filename):
+#         fm_filename = fm_dir + str(number_files+1)
+    
+#     shutil.copy(models_filename,fm_dir)
+    
+#     old_name = fm_dir + str(min_obj)
+#     os.rename(old_name,fm_filename)
+    
+#     # delete files in models_dir:
+#     for file_object in os.listdir(models_dir):
+#         file_object_path = os.path.join(models_dir, file_object)
+#         if os.path.isfile(file_object_path):
+#             os.unlink(file_object_path)
+
+#     return fm_filename
+
+
+# Test
+
+# hyperparameters = sum_of_squares_hp()
+# adaptive_lipo(10,100,hyperparameters,sum_of_squares)
+
